3e6df5f31ceb42e1f0bbb624ba9a77bd12664617
Zvonimir Fras <zvonimir.fras@ibm.com>
2018-03-29 10:55:42 -0400

    Fix test (import modal module)

--- src/app/home/home/home.component.spec.ts
+++ src/app/home/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { HomeComponent } from "./home.component";
+import { ModalModule } from "@peretz/neutrino";
 
 describe("HomeComponent", () => {
 	let component: HomeComponent;
@@ -8,6 +9,7 @@ describe("HomeComponent", () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			imports: [ ModalModule ],
 			declarations: [ HomeComponent ]
 		})
 		.compileComponents();
