913210669a38baf36378a5b84cd7c7fb79a29d11
Yousef Ouda <yousef@yousefs-mbp.torolab.ibm.com>
2018-11-30 13:55:26 -0500

    Fix tests

--- src/app/home/home/home.component.spec.ts
+++ src/app/home/home/home.component.spec.ts
@@ -2,12 +2,19 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { HomeComponent } from "./home.component";
 
+import { AccordionModule, ButtonModule, TabsModule } from "carbon-components-angular";
+
 describe("HomeComponent", () => {
 	let component: HomeComponent;
 	let fixture: ComponentFixture<HomeComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			imports: [
+				AccordionModule,
+				ButtonModule,
+				TabsModule
+			],
 			declarations: [ HomeComponent ]
 		})
 		.compileComponents();
