fce05f8fad076299aaf6d6bc1a247060ec639509
Cal Smith <calsmith@protonmail.com>
2019-09-26 11:54:59 -0400

    move service worker into main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,13 @@ if (environment.production) {
 	enableProdMode();
 }
 
+// Uncomment the following lines to activate the service worker
+// if (navigator.serviceWorker) {
+// 	navigator.serviceWorker.register('sw.js').then(() => {
+// 		console.log('Service worker installed')
+// 	}, err => {
+// 		console.error('Service worker error:', err);
+// 	});
+// }
+
 platformBrowserDynamic().bootstrapModule(AppModule);
