083bb5a860290aa40ad43a43b06abde7e48f1970
Cal Smith <calsmith@protonmail.com>
2019-11-07 11:00:52 -0500

    switch to hash router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, {useHash: true})],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
