382f915b8dc93ea19a597df64e47705cadca4579
Cal Smith <calsmith@protonmail.com>
2019-11-18 12:10:16 -0500

    init

--- src/app/home/landing-page/landing-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-landing-page',
-	templateUrl: './landing-page.component.html',
-	styleUrls: ['./landing-page.component.scss']
-})
-export class LandingPageComponent {
-}
--- src/app/repositories/repo-page/repo-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-repo-page',
-	templateUrl: './repo-page.component.html',
-	styleUrls: ['./repo-page.component.scss']
-})
-export class RepoPageComponent {
-}
