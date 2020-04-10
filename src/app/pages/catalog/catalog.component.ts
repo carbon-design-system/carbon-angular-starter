ca786f5bcc44ae49e3013be7c23ba1fc7faccd3b
Suren Konathala <suren@Surens-MacBook-Pro.local>
2020-04-10 08:52:42 -0500

    Included pages and content for catalog, docs, support and link1

---
+++ src/app/pages/catalog/catalog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.scss']
+})
+export class CatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
