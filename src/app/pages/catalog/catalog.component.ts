9c37a28a5c3a9b78c23c0f653a4280dda7724e45
Maicon Godinho <Maicon.Godinho@ibm.com>
2022-05-11 22:03:04 -0300

    chore: migrate from tslint to eslint
    Signed-off-by: Maicon Godinho <maicongodinho1@gmail.com>

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-catalog',
-  templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.scss']
+	selector: "app-catalog",
+	templateUrl: "./catalog.component.html",
+	styleUrls: ["./catalog.component.scss"],
 })
-export class CatalogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CatalogComponent {
+	constructor() {}
 }
