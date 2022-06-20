4f2b4635c8cd72fb032c6490d532e83e6b7934d6
Maicon Godinho <Maicon.Godinho@ibm.com>
2022-06-20 08:35:08 -0300

    fix: catch-all module declaration for icons
    Signed-off-by: Maicon Godinho <maicongodinho1@gmail.com>

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,1 @@
-// icon modules goes here for strict applications
-declare module '@carbon/icons/es/notification/16';
-declare module '@carbon/icons/es/user--avatar/16';
-declare module '@carbon/icons/es/apps/16';
+declare module '@carbon/icons/*';
