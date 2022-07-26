e5709d4143b95dcc41beeb81c5523dd4da159ba6
Maicon Godinho <maicongodinho1@gmail.com>
2022-07-26 04:23:56 -0300

    refac: use host-bind to space content with header
    Signed-off-by: Maicon Godinho <maicongodinho1@gmail.com>

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.scss']
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+	@HostBinding('class.bx--header') headerClass = true;
+
 	constructor() {}
 }
