d44ec9ae7a3fb473357d2e53fb6900e15ccc4419
Quasso <alex@forbettergood.ge>
2023-01-31 02:19:45 +0000

    this includes a bunch of tweaks and full package upgrades to use the latest version of angular/carbon and a more recent node version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { IconModule, IconService, UIShellModule } from 'carbon-components-angular';
 import Notification20 from '@carbon/icons/es/notification/20';
 import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
+import AppSwitcher20 from '@carbon/icons/es/app-connectivity/20';
 import { HeaderComponent } from './header/header.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { DocsComponent } from './pages/docs/docs.component';
